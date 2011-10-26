4fa2dca75cc92c3f5c253f7c4c740c98f20531c4
tamsuiboy <tamsuiboy@hotmail.com>
2011-10-26 17:03:29 +0800

    gh-pages updated

--- config.rb
+++ config.rb
@@ -2,10 +2,10 @@
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/lrthw-tw/"
+http_images_path = "/lrthw-tw/images"
+http_fonts_path = "/lrthw-tw/fonts"
+css_dir = "public/lrthw-tw/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
